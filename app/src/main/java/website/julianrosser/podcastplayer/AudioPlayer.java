4f3933f419aad2d2d54e1c854f70f96ce1f49cdb
Jules1991 <julianrosser91@gmail.com>
2015-05-11 16:00:05 +1000

    Tracks, SeekBar functionality & new Fragment class.

--- app/src/main/java/website/julianrosser/podcastplayer/AudioPlayer.java
+++ app/src/main/java/website/julianrosser/podcastplayer/AudioPlayer.java
@@ -2,19 +2,35 @@ package website.julianrosser.podcastplayer;
 
 import android.content.Context;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.util.Log;
+
+import java.util.Random;
 
 public class AudioPlayer {
 
-    private MediaPlayer mPlayer;
+    public Context mContext;
+    private static MediaPlayer mPlayer;
+    static boolean isPrepared;
 
-    public AudioPlayer(Context c) {
+    public AudioPlayer(Context c, Uri track) {
+        mContext = c;
         Log.i("context", "Context: " + c.toString());
-        mPlayer = MediaPlayer.create(c, R.raw.i);
+        mPlayer = MediaPlayer.create(c, track);
         Log.i("context", "mPlayer: " + mPlayer.toString());
+
+        isPrepared = false;
+        mPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                isPrepared = true;
+            }
+        });
+
         mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            public void onCompletion(MediaPlayer mp) {
-                stop();
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                setTrack(PlayerFragment.tracks[new Random().nextInt(PlayerFragment.tracks.length)]);
             }
         });
     }
@@ -22,7 +38,32 @@ public class AudioPlayer {
     public void play() {
         // Start MediaPlayer
         mPlayer.start();
+        getCheckPreparedThread().start();
     }
+
+    public void setTrack(Uri path) {
+
+        mPlayer.stop();
+        mPlayer.reset();
+        try {
+            mPlayer.setDataSource(mContext, path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            mPlayer.prepare();
+        } catch (Exception e2) {
+            e2.printStackTrace();
+        }
+        if (!mPlayer.isLooping()) {
+            mPlayer.start();
+        }
+
+        getCheckPreparedThread().start();
+
+
+    }
+
 
     public void pause() {
         mPlayer.pause();
@@ -30,6 +71,7 @@ public class AudioPlayer {
 
     public void resume() {
         mPlayer.start();
+        getCheckPreparedThread().start();
     }
 
     public void stop() {
@@ -42,19 +84,29 @@ public class AudioPlayer {
 
     public void rewind() {
         mPlayer.seekTo(getPlayTime() - 2500);
+        getCheckPreparedThread().start();
     }
 
     public void forward() {
         mPlayer.seekTo(getPlayTime() + 2500);
+
+        getCheckPreparedThread().start();
     }
 
     public void seekTo(int i) {
-        mPlayer.seekTo((this.getLength() / 100) * i);
+        mPlayer.seekTo((this.getLength() / 1000) * i);
     }
 
     public int getCurrentProgress() {
-        return mPlayer.getCurrentPosition();
+        Log.i(getClass().getSimpleName(), "Cur Pos: " + mPlayer.getCurrentPosition() + "  /  Dur  " + mPlayer.getDuration());
+        double pos = mPlayer.getCurrentPosition();
+        double dur = mPlayer.getDuration();
+        double prog = (pos / dur) * 1000;
+
+        return (int)Math.round(prog * 10) / 10;
     }
+
+
 
     public int getLength() {
         return mPlayer.getDuration();
@@ -68,12 +120,55 @@ public class AudioPlayer {
     }
 
     public Boolean getIsPlaying() {
-        if (null == mPlayer) {
-            return false;
-        } else {
-            return mPlayer.isPlaying();
-        }
+        return null != mPlayer && mPlayer.isPlaying();
 
+    }
+
+    public boolean getIsPrepared(){
+        return isPrepared;
+    }
+
+    /**
+     *
+     */
+    public static Thread getCheckPreparedThread() {
+        return new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 20; i > 0; i--) {
+                    if (!isPrepared) {
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        startProgressTracker();
+                        break;
+                    }
+                }
+            }
+        });
+    }
+
+    public static void startProgressTracker() {
+        Log.i("TAG", "startProgressTracker()");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (mPlayer != null && mPlayer.isPlaying()) {
+                    try {
+                        Thread.sleep(400);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    PlayerFragment.seekBar.setProgress(PlayerFragment.mPlayer.getCurrentProgress());
+                }
+                Log.i("TAG", "NOT PLAYING, SO STOP");
+
+            }
+        }).start();
     }
 
 }
